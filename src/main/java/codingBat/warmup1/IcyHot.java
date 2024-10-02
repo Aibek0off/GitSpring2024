f8e4e7059e4fcf488f03a45e193a94e425aa5d40
Aibek0off <aibek0off@gmail.com>
2024-10-02 17:39:24 +0600

    icyHot

---
+++ src/main/java/codingBat/warmup1/IcyHot.java
@@ -0,0 +1,7 @@
+package codingBat.warmup1;
+
+public class IcyHot {
+    public boolean icyHot(int temp1, int temp2) {
+        return (temp1 < 0 && temp2 > 100) || temp1 > 0 && temp2 < 100;
+    }
+}
