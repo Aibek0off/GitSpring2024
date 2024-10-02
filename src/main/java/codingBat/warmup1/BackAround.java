8574533a0487d89657dfa44d0fab9e9bbb87287b
Aibek0off <aibek0off@gmail.com>
2024-10-02 17:34:08 +0600

    backAround

---
+++ src/main/java/codingBat/warmup1/BackAround.java
@@ -0,0 +1,7 @@
+package codingBat.warmup1;
+
+public class BackAround {
+    public String backAround(String str) {
+        return str.substring(str.length()-1) + str + str.substring(str.length()-1);
+    }
+}
