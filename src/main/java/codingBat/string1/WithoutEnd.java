2556cda790c46d45d70c17a40a1c2021038336fe
Aibek0off <aibek0off@gmail.com>
2024-10-05 19:57:21 +0600

    withoutEnd

---
+++ src/main/java/codingBat/string1/WithoutEnd.java
@@ -0,0 +1,7 @@
+package codingBat.string1;
+
+public class WithoutEnd {
+    public String withoutEnd(String str) {
+        return str.length() >= 2 ? str.substring(1, str.length()-1) : str;
+    }
+}
